7fba26fc7b4997c1106fdd46d78ff163c3c601b7
Alan Wu <lunw1024@gmail.com>
2022-06-11 00:24:35 +0000

    responsive

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,25 +6,25 @@ import { Icon } from '@iconify/vue'
 
 <template>
   <div class="navbar rounded-2xl w-auto">
-    <div class="navbar-start">
+    <div class="navbar-start flex items-center">
       <div class="dropdown md:hidden">
-        <label tabindex="0" class="btn btn-ghost btn-square rounded-lg">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+        <label tabindex="0" class="btn btn-ghost btn-md btn-square rounded-lg">
+          <Icon icon="charm:menu-hamburger" class="h-5 w-5" />
         </label>
-        <ul tabindex="0" class="menu dropdown-content p-2 border-2 border-black rounded-2xl">
+        <ul tabindex="0" class="menu dropdown-content p-2 border-2 border-black rounded-2xl bg-white/80">
           <li><NuxtLink class="btn btn-ghost btn-sm" to="/blog">Blog</NuxtLink></li>
           <li><NuxtLink class="btn btn-ghost btn-sm" to="/projects">Projects</NuxtLink></li>
           <li><a class="btn btn-ghost btn-sm">Resume</a></li>
           <li><NuxtLink class="btn btn-ghost btn-sm" to="/contacts">Contact</NuxtLink></li>
         </ul>
       </div>
-      <NuxtLink class="btn btn-ghost btn-circle text-lg font-bold mx-4 p-2" to="/">
+      <NuxtLink class="btn btn-ghost btn-square btn-md text-lg font-bold mx-4 p-2" to="/">
         <img :src="favicon" alt="AW" class="object-contain">
       </NuxtLink>
     </div>
     <div class="navbar-center"> </div>
     <div class="navbar-end">
-      <div class="flex hidden md:inline items-center ">
+      <div class="flex hidden md:inline items-center">
         <NuxtLink class="btn btn-ghost btn-sm rounded-lg align-middle" to="/blog">Blog</NuxtLink>
         <NuxtLink class="btn btn-ghost btn-sm rounded-lg align-middle" to="/projects">Projects</NuxtLink>
         <a class="btn btn-ghost btn-sm rounded-lg align-middle">Resume</a>
