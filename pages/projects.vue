b08c9a7128c30ad7b33684ba6440043439b8df23
Alan Wu <lunw1024@gmail.com>
2022-07-17 06:35:27 +0000

    scpt, terminal

--- pages/projects.vue
+++ pages/projects.vue
@@ -3,9 +3,39 @@
 
 <template>
   <TextBox>
+    <!-- Terminal -->
+    <h1 class="mx-auto font-semibold text-2xl">Terminal Global Championship</h1>
+    <p> Working with
+      <NuxtLink class="link link-primary link-hover" to="https://www.linkedin.com/in/sahilrane/" target="_blank">Sahil Rane</NuxtLink> and <NuxtLink class="link link-primary link-hover" to="https://www.linkedin.com/in/milo-knell/" target="_blank">Milo Knell</NuxtLink>, I competed in <NuxtLink class="link link-primary link-hover" to="https://terminal.c1games.com/home" target="_blank">Terminal Global Championship</NuxtLink> In the contest, we built an AI agent to play an incomplete information game with a pool of opponents. In the final round robin, our agent won against the first place team from the University of Warsaw (JKS and the legendary programmer, <NuxtLink class="link link-primary link-hover" to="https://www.youtube.com/c/Errichto" target="_blank">Errichto</NuxtLink>.) However, we ended up in second place due to the tiebreaker, taking down $10,000 in cash.
+    </p>
+    <img class="rounded-2xl mx-auto" src="~/assets/img/terminal.png" alt="Terminal" />
+    <div class="divider py-10"></div>
+
+    <!-- SCPT -->
+    <h1 class="mx-auto font-semibold text-2xl">Socal College Poker Tournament</h1>
+    <p>Teaming with
+      <NuxtLink class="link link-primary link-hover" to="https://www.linkedin.com/in/tiger-che/" target="_blank">Tiger Che</NuxtLink>, <NuxtLink class="link link-primary link-hover" to="https://www.linkedin.com/in/sahilrane/" target="_blank">Sahil Rane</NuxtLink>, and <NuxtLink class="link link-primary link-hover" to="https://www.linkedin.com/in/laney-goldman-33aa19228/" target="_blank">Laney Goldman</NuxtLink> as Team Frosh, I competed in the <NuxtLink class="link link-primary link-hover" to="https://www.socalcollegepoker.com/" target="_blank">Socal College Poker Tournament</NuxtLink> at Hilton Pasadena. Making my way to the final table, I ended up taking the individual 8th place.
+    </p>
+    <div>
+      <p class="space-x-2"><span class="badge badge-outline">Article</span>
+        <NuxtLink class="link link-primary link-hover" to="https://www.linkedin.com/posts/socal-college-poker-tournament_were-very-happy-to-announce-that-the-first-activity-6923770231567892480-qiMM" target="_blank">Linkedin Post</NuxtLink>
+      </p>
+      <p class="space-x-2"><span class="badge badge-outline">Article</span>
+        <NuxtLink class="link link-primary link-hover" to="https://caltechadmissions.blog/socal-college-poker-tournament/">Caltech Admission</NuxtLink>
+      </p>
+    </div>
+    
+    <div class="grid grid-cols-2 gap-2 mx-auto">
+      <img class="rounded-2xl m-auto" src="~/assets/img/scpt1.jpeg" alt="scpt1" />
+      <img class="rounded-2xl m-auto" src="~/assets/img/scpt2.jpeg" alt="scpt2" />
+      <img class="rounded-2xl col-span-2" src="~/assets/img/scpt3.png" alt="scpt3" />
+    </div>
+    <div class="divider py-10"></div>
+
+    <!-- Chainlink -->
     <h1 class="mx-auto font-semibold text-2xl">Chainlink Hackathon</h1>
     <p>Working with
-      <NuxtLink class="link link-primary link-hover" to="https://www.linkedin.com/in/tiger-che/" target="_blank">Tiger Che</NuxtLink> and <NuxtLink class="link link-primary link-hover" to="https://www.linkedin.com/in/john-hearn-8434221a8/" target="_blank">John Hearn</NuxtLink>, I entered for Chainlink Spring Hackathon 2022. Using blockchain technologies, we built a platform for NFT collateralized p2p lendings. Our project won the top quality project award (top 40), and we received a $750 cash prize.
+      <NuxtLink class="link link-primary link-hover" to="https://www.linkedin.com/in/tiger-che/" target="_blank">Tiger Che</NuxtLink> and <NuxtLink class="link link-primary link-hover" to="https://www.linkedin.com/in/john-hearn-8434221a8/" target="_blank">John Hearn</NuxtLink>, I entered for Chainlink Spring Hackathon 2022. Using blockchain technologies, we built a platform for NFT collateralized p2p lendings. Our project won the top quality project award (top 40), and we received a $750 cash prize. We're currently in contact with ChainlinkLabs 
     </p>
     <div>
       <p class="space-x-2"><span class="badge badge-outline">Project</span>
@@ -18,23 +48,26 @@
         <NuxtLink class="link link-primary link-hover" to="https://github.com/jomahe/NFT-Loans">Github Repo</NuxtLink>
       </p>
     </div>
-    <img class="mx-auto" src="~/assets/img/chainlink-hackathon.svg" alt="Citadel Data Open Image" />
-    <img class="rounded-2xl mx-auto" src="~/assets/img/collateral.png" alt="Presentation Slide" />
+    <div class="grid grid-cols-2 gap-2 mx-auto">
+      <img class="m-auto" src="~/assets/img/chainlink-hackathon.svg" alt="Citadel Data Open Image" />
+      <img class="rounded-2xl m-auto" src="~/assets/img/collateral.png" alt="Presentation Slide" />
+    </div>
 
     <div class="divider py-10"></div>
 
+    <!-- AMISTAD -->
     <h1 class="mx-auto font-semibold text-2xl">Theoretical ML Research</h1>
-    <p>I joined the <NuxtLink class="link link-primary link-hover" to="https://www.cs.hmc.edu/~montanez/amistad.html" target="_blank">AMISTAD Lab</NuxtLink> in Summer 2022, and conducted research on the Algorithmic Search Framwork with Prof George Montanez.
+    <p>I joined the <NuxtLink class="link link-primary link-hover" to="https://www.cs.hmc.edu/~montanez/amistad.html" target="_blank">AMISTAD Lab</NuxtLink> in Summer 2022, and conducted research on theoretical ML with Prof George Montanez.
     </p>
     <div>
       <p class="space-x-2"><span class="badge badge-outline">Paper</span>
         <NuxtLink class="link link-primary link-hover" to="https://www.mcdonalds.com/us/en-us.html" target="_blank">TBA</NuxtLink>
       </p>
     </div>
-    <img class="rounded-2xl mx-auto" src="~/assets/img/amistad.jpeg" alt="AMISTAD Logo" />
-
+    <img class="rounded-2xl m-auto" src="~/assets/img/amistad.jpeg" alt="amistad" />
     <div class="divider py-10"></div>
 
+    <!-- Datathon -->
     <h1 class="mx-auto font-semibold text-2xl">Citadel Data Open</h1>
     <p>Working with
       <NuxtLink class="link link-primary link-hover" to="https://www.linkedin.com/in/milo-knell/" target="_blank">Milo Knell</NuxtLink>, <NuxtLink class="link link-primary link-hover" to="https://www.linkedin.com/in/forrestbicker/" target="_blank">Forrest Bicker</NuxtLink>, and <NuxtLink class="link link-primary link-hover" to="https://www.linkedin.com/in/dchen327/" target="_blank">David Chen</NuxtLink>, I entered for Citadel West Coast Data Open with a paper about NLP based clickbait detection. We used a emotion-based fake news detector to examine the business behavior of a large media company, Upworthy.com. We discovered significant relations between fake news and the A/B test results of Upworthy, and confirmed that relation in comparing the trend of clickbait production and Upworthy's pledge to shift their business focus. Ultimately, our team placed first in the contest, and we received a $10,000 cash prize and an interview offer at Citadel.
@@ -52,6 +85,7 @@
 
     <div class="divider py-10"></div>
 
+    <!-- Yau Award -->
     <h1 class="mx-auto font-semibold text-2xl">Computer Vision Research</h1>
     <p> I conducted research on computer vision during high school and published a paper about multi-modal feature fusion in the context of autonomous driving. The paper proposed a method to fuse LiDAR point clouds and RGB image sequences, and an algorithm to establish dense correspondence between feature maps of space-invariant networks. I was invited to perform a oral presentation at Tsinghua University. The paper and the presentation won the Bronze prize (global 3rd) of the S.T. Yau High School Science Award with ¥10,000 in cash, and a reccommendation letter from the great mathematician, Fields medalist, Shing-Tung Yau.
     </p>
@@ -63,8 +97,11 @@
         <NuxtLink class="link link-primary link-hover" to="https://github.com/lunw1024/DenseFuseNet">Github Repo</NuxtLink>
       </p>
     </div>
-    <img class="rounded-2xl mx-auto" src="~/assets/img/DenseFuseNet-result.gif" alt="DenseFuseNet Output" />
-    <img class="rounded-2xl mx-auto" src="~/assets/img/yau.jpeg" alt="Yau Photo" />
+    <div class="grid grid-cols-2 gap-2 mx-auto">
+      <img class="rounded-2xl m-auto" src="~/assets/img/DenseFuseNet-result.gif" alt="DenseFuseNet Output" />
+      <img class="rounded-2xl m-auto" src="~/assets/img/yau.jpeg" alt="Yau Photo" />
+    </div>
+
   </TextBox>
 
 </template>
